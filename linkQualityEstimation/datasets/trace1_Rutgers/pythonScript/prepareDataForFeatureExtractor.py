c4cacce4c5523bf13d97f106bf67642a2e278857
Timotej Gale <timotej.gale@gmail.com>
2016-11-22 10:41:47 +0100

    Rutgers: Add white noise as RSSI of missing packets

--- linkQualityEstimation/datasets/trace1_Rutgers/pythonScript/prepareDataForFeatureExtractor.py
+++ linkQualityEstimation/datasets/trace1_Rutgers/pythonScript/prepareDataForFeatureExtractor.py
@@ -3,10 +3,12 @@ import sys
 import pandas as pd
 from shutil import rmtree
 from natsort import natsorted
+import numpy as np
+import sys
 
 PATH = "../data/"
 FEATURE_EXTRACTOR_DATASETS = "../../../featureGenerator/datasets/dataset-2-rutgers_wifi/"
-NUMBER_OF_PACKETS = 300.0
+NUMBER_OF_PACKETS = 300
 RSSI_MAX = 127
 RSSI_MIN = 0
 NUMBER_OF_BINS = 40
@@ -33,6 +35,8 @@ for root, dirs, files in natsorted(os.walk(PATH)):
         if file.endswith(""):
             x, y, z = [], [], []
             current_seq = 0
+            previous_rssi = RSSI_MIN
+            previous_seq = -1
             missing_values = 0
             fiveNumSummary = 0
             name = dir[1].split("_")
@@ -42,21 +46,29 @@ for root, dirs, files in natsorted(os.walk(PATH)):
             for line in file:
                 data = line.split()
                 if len(data) > 1:
-                    if int(data[0]) < NUMBER_OF_PACKETS and int(data[1]) <= RSSI_MAX and int(data[1]) >= RSSI_MIN:
-                        while current_seq < int(data[0]):
-                            x.append(current_seq)
-                            y.append(RSSI_MIN)
-                            z.append("false")
-                            current_seq += 1
-                        x.append(data[0])
-                        y.append(int(data[1]))
+                    rssi = int(data[1])
+                    seq = int(data[0])
+                    if seq < NUMBER_OF_PACKETS and rssi <= RSSI_MAX and rssi >= RSSI_MIN:
+                        avg_rssi = np.mean((previous_rssi, rssi))
+                        std_rssi = np.std((previous_rssi, rssi))
+                        std_rssi = std_rssi if std_rssi > 0 else sys.float_info.min
+                        difference = seq - previous_seq - 1
+                        x.extend(range(previous_seq + 1, int(data[0])))
+                        y.extend(np.random.normal(avg_rssi, std_rssi, size=difference))
+                        z.extend(["false"] * difference)
+                        
+                        x.append(seq)
+                        y.append(rssi)
                         z.append("true")
-                        current_seq += 1
-            while current_seq < NUMBER_OF_PACKETS:
-                x.append(current_seq)
-                y.append(RSSI_MIN)
-                z.append("false")
-                current_seq += 1
+                        previous_seq = seq
+                        previous_rssi = rssi
+            difference = NUMBER_OF_PACKETS - previous_seq - 1
+            avg_rssi = np.mean((previous_rssi, RSSI_MIN))
+            std_rssi = np.std((previous_rssi, RSSI_MIN))
+            std_rssi = std_rssi if std_rssi > 0 else sys.float_info.min
+            x.extend(range(previous_seq + 1, NUMBER_OF_PACKETS))
+            y.extend(np.random.normal(avg_rssi, std_rssi, size=difference))
+            z.extend(["false"] * difference)
 
             file.close()
             #missing_values = (NUMBER_OF_PACKETS - len(y))
