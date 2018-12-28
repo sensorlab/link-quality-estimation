f73b0287b85ab39981ed884eeeb88bd95f3dec8a
Gregor Cerar <grega90@gmail.com>
2018-12-28 16:03:12 +0100

    Updated transformation script

--- datasets/trace1_Rutgers/scripts/transform.py
+++ datasets/trace1_Rutgers/scripts/transform.py
@@ -1,84 +1,91 @@
-import os
-import sys
-import pandas as pd
-from shutil import rmtree
-from natsort import natsorted
+from typing import List, Tuple
+from glob import glob
+from os import makedirs, path
+import multiprocessing as mp
 import numpy as np
+import pandas as pd
 
-PATH = "../data/"
-FEATURE_EXTRACTOR_DATASETS = "../../../featureGenerator/datasets/dataset-2-rutgers_wifi/"
-NUMBER_OF_PACKETS = 300
-RSSI_MAX = 127
-RSSI_MIN = 0
+RUTGERS_SCRIPT_ROOT = path.dirname(path.abspath(__file__))
+RUTGERS_ROOT = path.dirname(RUTGERS_SCRIPT_ROOT)
+DATASETS_ROOT = path.dirname(RUTGERS_ROOT)
+PROJECT_ROOT = path.dirname(DATASETS_ROOT)
 
-number_experiment = 0
-tmp_dir = ""
+RUTGERS_TRACES = path.join(RUTGERS_ROOT, 'data', '**', 'sdec*')
 
-# Create dataset directory
-if os.path.exists(FEATURE_EXTRACTOR_DATASETS):
-    rmtree(FEATURE_EXTRACTOR_DATASETS)
-os.makedirs(FEATURE_EXTRACTOR_DATASETS)
-
-for root, dirs, files in natsorted(os.walk(PATH)):
-    root = root.lstrip()
-    print root
-    tmp = root[len(PATH):]
-    dir = tmp.split("/")
-    for file in files:
-        if file.endswith(""):
-            x, y, z = [], [], []
-            current_seq = 0
-            previous_rssi = RSSI_MIN
-            previous_seq = -1
-
-            name = dir[1].split("_")
-            from_node = name[1].replace("-", "_")
-            to_node = str("node" + file[len("sdec"):]).replace("-", "_")
-            file = open(os.path.join(root, file), 'r')
-            for line in file:
-                data = line.split()
-                if len(data) > 1:
-                    rssi = int(data[1])
-                    seq = int(data[0])
-                    if seq < NUMBER_OF_PACKETS and rssi <= RSSI_MAX and rssi >= RSSI_MIN:
-                        # Interpolate
-                        avg_rssi = np.mean((previous_rssi, rssi))
-                        std_rssi = np.std((previous_rssi, rssi))
-                        std_rssi = std_rssi if std_rssi > 0 else sys.float_info.min
-                        difference = seq - previous_seq - 1
-                        x.extend(range(previous_seq + 1, int(data[0])))
-                        y.extend(np.random.normal(avg_rssi, std_rssi, size=difference))
-                        z.extend(["false"] * difference)
-                        
-                        x.append(seq)
-                        y.append(rssi)
-                        z.append("true")
-                        previous_seq = seq
-                        previous_rssi = rssi
-            # Interpolate
-            difference = NUMBER_OF_PACKETS - previous_seq - 1
-            avg_rssi = np.mean((previous_rssi, RSSI_MIN))
-            std_rssi = np.std((previous_rssi, RSSI_MIN))
-            std_rssi = std_rssi if std_rssi > 0 else sys.float_info.min
-            x.extend(range(previous_seq + 1, NUMBER_OF_PACKETS))
-            y.extend(np.random.normal(avg_rssi, std_rssi, size=difference))
-            z.extend(["false"] * difference)
-
-            file.close()
+OUTPUT_DIR = path.join(PROJECT_ROOT, 'featureGenerator', 'datasets', 'dataset-2-rutgers_wifi')
 
 
-            # Create directory path if it is missing
-            if tmp_dir != dir[0]:
-                number_experiment += 1
-            experiment_path = FEATURE_EXTRACTOR_DATASETS + "experiment-" + str(number_experiment) + "-noise_level_" + dir[0].replace("-", "_")
-            if not os.path.exists(experiment_path):
-                tmp_dir = dir[0]
-                os.makedirs(experiment_path)
+def ensure_dir(file_path: str) -> None:
+    directory = path.dirname(file_path)
+    if not path.exists(directory):
+        makedirs(directory)
 
-            # Create csv file per link
-            csv_file = experiment_path + "/trans-" + from_node + "-recv-" + to_node + ".csv"
-            print "trans-" + from_node + "-recv-" + to_node + ".csv"
-            df = pd.DataFrame({"seq": x, "rssi": y, "received": z})
-            # Keep colum order
-            df = df[['seq', 'rssi', 'received']]
-            df.to_csv(csv_file, index=False, header=True, cols=['seq', 'rssi', 'received'])
+    return file_path
+
+
+def parse_rutgers(filename: str) -> pd.DataFrame:
+    DTYPE = np.float32
+
+    noise = int(filename.split('/')[-3][len('dbm'):])
+    src = 'node' + filename.split('/')[-1][len('sdec'):]
+    dst = filename.split('/')[-2].split('_')[1]
+
+    data = np.full(shape=300, fill_value=np.NaN, dtype=DTYPE)
+    with open(filename, 'r') as f:
+        for line in f:
+            d = line.split()
+            if len(d) >= 2:
+                seq, rssi = int(d[0]), int(d[1])
+                if seq < 300 and rssi < 128 and rssi >= 0:
+                    data[seq] = rssi
+
+    df = pd.DataFrame(data=data, columns=['rssi'])
+    df['seq'] = df.index + 1
+    df['noise'] = noise
+    df['src'] = src
+    df['dst'] = dst
+    df['received'] = ~df['rssi'].isnull()
+
+    #df = df.set_index('seq')
+
+    return df
+
+
+def load_rutgers() -> List[pd.DataFrame]:
+    """Will parse all traces. The output is list of Pandas DataFrame.
+
+    Traces are not processed and contain missing fields
+    """
+    files = glob(RUTGERS_TRACES, recursive=True)
+    traces = [parse_rutgers(df) for df in files]
+    return traces
+
+
+def load_rutgers_parallel() -> List[pd.DataFrame]:
+    """Will parse all traces in parallel. The output is list/tuple of Pandas DataFrame.
+
+    Traces are not processed and contain missing fields
+    """
+    files = glob(RUTGERS_TRACES, recursive=True)
+    with mp.Pool() as p:
+        traces = p.map(parse_rutgers, files)
+
+    return traces
+
+
+# Noise to experiment number mapping
+# Just to comply with output directory name
+if __name__ == '__main__':
+    links = load_rutgers_parallel()
+    for link in links:
+        noise = link['noise'].values[0]
+        src = link['src'].values[0]
+        dst = link['dst'].values[0]
+        link.to_csv(
+            ensure_dir(f'{OUTPUT_DIR}/experiment-{abs(noise // 5) + 1}-noise_level_{abs(noise)}/{src}-{dst}-{noise}dBm.csv'),
+            index=False,
+            columns=['seq', 'src', 'dst', 'noise',  'received', 'rssi'],
+        )
+
+
+    print('Done!', len(links), 'traces')
