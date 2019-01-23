80468a0d923925ef7c4e3123536bd830e2588173
Gregor Cerar <grega90@gmail.com>
2019-01-23 14:57:57 +0100

    Added CACHE_PATH constant.
    It serves as common path to cache directory on project root to be used
    with e.g. joblib.Memory cache.

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -4,3 +4,5 @@ TRACES_PATH = path.dirname(path.abspath(__file__))
 PROJECT_PATH = path.dirname(TRACES_PATH)
 
 TRANSFORM_OUTPUT_PATH = path.join(PROJECT_PATH, 'featureGenerator', 'datasets')
+
+CACHE_PATH = path.join(PROJECT_PATH, '.cache')
