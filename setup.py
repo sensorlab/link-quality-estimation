19ecef22eafbc94e240096db4e9cec24cb8e10f2
Gregor Cerar <grega90@gmail.com>
2019-01-23 14:47:04 +0100

    Added setup.py and updated requirements.txt accordingly.
    With 'pip install -e .' or 'pip install -r requirements.txt' command,
    we avoid hacking with sys.path in order import packages.

---
+++ setup.py
@@ -0,0 +1,37 @@
+from os import path
+
+from setuptools import setup
+
+
+def read(fname):
+    with open(path.join(path.dirname(__file__), fname)) as f:
+        return f.read()
+
+requirements = [
+    'numpy',
+    'scipy',
+    'pandas',
+    'matplotlib',
+    'seaborn',
+]
+
+setup(
+    name='LQE',
+    version='0.1',
+    url='https://github.com/sensorlab/link-quality-estimation',
+    author='SensorLab',
+    author_email='sensorlab@ijs.si',
+    description='Collection of link quality estimation (LQE) trace-sets for reasearchers',
+    long_description=read('README.md'),
+    license='GPL3+',
+    packages=['datasets'],
+    include_package_data=True,
+    python_requires='>=3.6',
+    install_requires=requirements,
+    extras_require={
+        'dev': ['pylint', 'autopep8'],
+        'ml': ['scikit-learn<0.20', 'imbalanced-learn', 'sklearn-pandas', 'scikit-image', 'joblib'],
+        'lab': ['jupyterlab', 'joblib'],
+        'cluster': ['ipyparallel'],
+    },
+)
