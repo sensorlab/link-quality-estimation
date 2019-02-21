cb2200fabb7076d416405906ca02675ebbcd168a
Gregor Cerar <grega90@gmail.com>
2019-02-21 15:23:07 +0100

    Since featureGenerator was removed, output of transform scripts will be './output/datasets/<dataset>'

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,6 +3,6 @@ from os import path
 TRACES_PATH = path.dirname(path.abspath(__file__))
 PROJECT_PATH = path.dirname(TRACES_PATH)
 
-TRANSFORM_OUTPUT_PATH = path.join(PROJECT_PATH, 'featureGenerator', 'datasets')
+TRANSFORM_OUTPUT_PATH = path.join(PROJECT_PATH, 'output', 'datasets')
 
 CACHE_PATH = path.join(PROJECT_PATH, '.cache')
